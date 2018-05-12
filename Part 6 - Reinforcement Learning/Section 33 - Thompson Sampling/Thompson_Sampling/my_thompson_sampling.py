53d576b89bbf429954bfb42db4498d22749849bb
Michael Mattox <michael.mattox@gmail.com>
2018-05-12 18:08:22 +0200

    thompson

--- Part 6 - Reinforcement Learning/Section 33 - Thompson Sampling/Thompson_Sampling/my_thompson_sampling.py
+++ Part 6 - Reinforcement Learning/Section 33 - Thompson Sampling/Thompson_Sampling/my_thompson_sampling.py
@@ -9,33 +9,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import math
+import random
 
 dataset = pd.read_csv("Ads_CTR_Optimisation.csv")
 
 ROUNDS = 10000
 ADS = 10
 
-numbers_of_selections = [0] * ADS
-sums_of_rewards = [0] * ADS
 ads_selected = []
 total_reward = 0
+
+numbers_of_rewards_1 = [0] * ADS
+numbers_of_rewards_0 = [0] * ADS
+
 for n in range(0, ROUNDS):
-    max_upper_bound = 0
     ad = 0
+    max_random = 0
     for i in range(0, ADS): 
-        if numbers_of_selections[i] > 0:
-            average_reward = sums_of_rewards[i] / numbers_of_selections[i]
-            delta_i = math.sqrt(3/2 * math.log(n+1) / numbers_of_selections[i])
-            upper_bound = average_reward + delta_i
-        else : 
-            upper_bound = 1e400
-        if upper_bound > max_upper_bound:
-            max_upper_bound = upper_bound
+        random_beta = random.betavariate(numbers_of_rewards_1[i] + 1, numbers_of_rewards_0[i] + 1)
+        if random_beta > max_random:
+            max_random = random_beta
             ad = i
     ads_selected.append(ad)
-    numbers_of_selections[ad] = numbers_of_selections[ad] + 1
     reward = dataset.values[n,ad]
-    sums_of_rewards[ad] = sums_of_rewards[ad] + reward
+    if reward == 1:
+        numbers_of_rewards_1[ad] = numbers_of_rewards_1[ad] + 1
+    else:
+        numbers_of_rewards_0[ad] = numbers_of_rewards_0[ad] + 1
     total_reward = total_reward + reward
     
 plt.hist(ads_selected)
